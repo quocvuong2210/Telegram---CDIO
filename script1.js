25cb5e9074cad728b919f1e3777edb721145f9af
Đăng Khoa <bipro29082005@gmail.com>
2026-03-05 22:15:04 +0700

    no message

--- script1.js
+++ script1.js
@@ -3,17 +3,16 @@ document.addEventListener("DOMContentLoaded", function() {
     const loginForm = document.getElementById("login-form");
     if (loginForm) {
         loginForm.addEventListener("submit", function(event) {
-            event.preventDefault(); // Ngăn trang load lại
+            event.preventDefault();
             const phone = document.getElementById("login-phone").value;
             alert("Đang xử lý đăng nhập cho số: +84 " + phone);
         });
     }
 
-    // Xử lý form đăng ký
     const registerForm = document.getElementById("register-form");
     if (registerForm) {
         registerForm.addEventListener("submit", function(event) {
-            event.preventDefault(); // Ngăn trang load lại
+            event.preventDefault();
             const phone = document.getElementById("reg-phone").value;
             const firstName = document.getElementById("first-name").value;
             alert(
